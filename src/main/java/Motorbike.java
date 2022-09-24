ec7445257f446efaf980254480cbef4114153080
Evgeny Smorzhevskij <esmorzevskij@gmail.com>
2022-09-24 21:42:56 +0300

    second commit Marafon

---
+++ src/main/java/Motorbike.java
@@ -0,0 +1,16 @@
+public class Motorbike {
+   String model;
+   String color;
+  int age;
+  public Motorbike( String model,String color,int age){
+    this.model = model;
+    this.color = color;
+    this.age = age;
+  }
+  public void info(){
+    System.out.println("It's bike");
+  }
+  public int yearDifference(int inputYear){
+    return inputYear - age;
+  }
+}
