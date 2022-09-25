bc369d78b07784ae17fabbb8eb6954a5bc363bbc
Evgeny Smorzhevskij <esmorzevskij@gmail.com>
2022-09-25 11:15:45 +0300

    Day 9.Task 9

---
+++ src/main/java/Figure.java
@@ -0,0 +1,15 @@
+abstract class Figure {
+  private  String color;
+
+  public Figure(String color){
+    this.color = color;
+  }
+  public String getColor(){
+    return color;
+  }
+  public abstract double area();
+  public abstract double perimeter();
+
+
+
+}
